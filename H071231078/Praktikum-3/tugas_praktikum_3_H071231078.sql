0ce715c7a8b4f70dccb66d2b9d64e7a3b63daf05
ivy799 <muhammadraihanaan123@gmail.com>
2024-09-27 09:53:47 +0800

    tugas praktikum 3 Muhammad Riahan H071231078

---
+++ H071231078/Praktikum-3/tugas_praktikum_3_H071231078.sql
@@ -0,0 +1,37 @@
+-- SOAL 1
+-- INSERT INTO authors (NAME, nationality)
+-- 	VALUES("Tere Liye", "Indonesian"),
+-- 			("J.K.Rowling", "British"),
+-- 			("Andrea Hirata",NULL);
+-- 
+-- INSERT INTO books (isbn, title, author_id, published_Year, genre, copies_available)
+-- 	VALUES("7040289780375", "Ayah", 3, 2015, "Fiction", 15),
+-- 			("9780375704025", "Bumi", 1, 2014, "Fantasy", 5),
+-- 			("8310371703024", "Bulan", 1, 2015, "Fantasy", 3),
+-- 			("9780747632699", "Harry Potter and the Philosopher's Stone", 2, 1997, NULL , 10),
+-- 			("7210301703022", "The Running Grave", 2, 2016, "Fiction", 11);
+-- 			
+-- INSERT INTO members (first_name, last_name, email, phone_number, join_date, membership_type)
+-- 	VALUES("John","Doe","John.doe@example.com",NULL,'2023-04-29',NULL),
+-- 			("Alice","Johnson","Alice.johnson@example.com",1231231231,'2023-05-01',"Standar"),
+-- 			("Bob","Williams","bob.williams@example.com",3213214321,'2023-06-20',"Premium");
+-- 			
+-- INSERT INTO borrowings (member_id,book_id,borrow_date,return_date)
+-- 	VALUES(1,4,'2023-07-10','2023-07-25'),
+-- 			(3,1,'2023-08-01',NULL),
+-- 			(2,5,'2023-09-06','2023-09-09'),
+-- 			(2,3,'2023-09-08',NULL),
+-- 			(3,2,'2023-09-10',NULL);
+-- 
+-- SOAL 2
+-- UPDATE books
+-- SET copies_available = copies_available - 1
+-- WHERE id IN(1,2,3);
+-- 
+-- SOAL 3
+-- UPDATE members
+-- SET membership_type = "Standar"
+-- WHERE id IN(3);
+-- 
+-- DELETE from members
+-- WHERE id IN(2);
